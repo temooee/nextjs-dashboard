7885e29189f47166a3ac9354d90ff61eecedbf0c
Temo Vashakidze <temooee@gmail.com>
2024-09-23 21:02:30 +0400

    Added pagination and debouncing to search

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import {string} from "zod";
+import {fetchInvoicesPages} from "@/app/lib/data";
 
 type InvoicePageParams = {
     searchParams?: {
@@ -17,6 +18,7 @@ type InvoicePageParams = {
 export default async function Page({searchParams}: InvoicePageParams) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -30,7 +32,7 @@ export default async function Page({searchParams}: InvoicePageParams) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                 <Pagination totalPages={totalPages}/>
             </div>
         </div>
     );
